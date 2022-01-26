e184f0afbc7c4bcf4b27eebcf497f2948bd8c2d1
elizabethmroz <elizabethjmroz@gmail.com>
2022-01-26 11:28:56 +0000

    List for coordinates, Agent 1
    Removed unnecessary variables for Agent 0 and Agent 1; adding coordinates tuples directly to list instead.

--- ABM_sheep.py
+++ ABM_sheep.py
@@ -8,21 +8,19 @@ Created on Tue Jan 25 12:39:06 2022
 import random 
 import math
 
+# list created for coordinates
 agents = []
 
-# xy starting variables
-y0 = random.randint(0, 99) 
-x0 = random.randint(0,99)
 
-# xy starting variables
-y1 = random.randint(0,99) 
-x1 = random.randint(0,99)
+#c coordinates for agents generated, assigned to list
+agents.append([random.randint(0, 99),random.randint(0, 99)])
+agents.append([random.randint(0, 99),random.randint(0, 99)])
 
-agents.append([y0,x0])
+print(agents)
 
 # print variables
 print("starting position of agent 0 is", agents[0][0], ",", agents[0][1])
-print("starting position of agent 1 is", y1, ",", x1)
+print("starting position of agent 1 is", agents[1][0], ",", agents[1][1])
 
 # TIMESTEP 1
 # alter y randomly by +-1 (sheep walk one step)
@@ -95,76 +93,76 @@ else:
 #TIMESTEP 1
 # alter y randomly by +-1 (sheep walk one step)
 if random.random() <0.5:
-    y1 = y1 + 1
+    agents[1][0] = agents[1][0] + 1
 else: 
-    y1 = y1 - 1
+    agents[1][0] = agents[1][0] - 1
     
 # alter x randomly by +-1
 if random.random() <0.5:
-    x1 = x1 + 1
+    agents[1][1] = agents[1][1] + 1
 else: 
-    x1 = x1 - 1
+    agents[1][1] = agents[1][1] - 1
     
 #TIMESTEP 2
 # alter y randomly by +-1 (sheep walk one step)
 if random.random() <0.5:
-    y1 = y1 + 1
+    agents[1][0] = agents[1][0] + 1
 else: 
-    y1 = y1 - 1
+    agents[1][0] = agents[1][0] - 1
     
 # alter x randomly by +-1
 if random.random() <0.5:
-    x1 = x1 + 1
+    agents[1][1] = agents[1][1] + 1
 else: 
-    x1 = x1 - 1
+    agents[1][1] = agents[1][1] - 1
     
 #TIMESTEP 3
 # alter y randomly by +-1 (sheep walk one step)
 if random.random() <0.5:
-    y1 = y1 + 1
+    agents[1][0] = agents[1][0] + 1
 else: 
-    y1 = y1 - 1
+    agents[1][0] = agents[1][0] - 1
     
 # alter x randomly by +-1
 if random.random() <0.5:
-    x1 = x1 + 1
+    agents[1][1] = agents[1][1] + 1
 else: 
-    x1 = x1 - 1
+    agents[1][1] = agents[1][1] - 1
     
 #TIMESTEP 4
 # alter y randomly by +-1 (sheep walk one step)
 if random.random() <0.5:
-    y1 = y1 + 1
+    agents[1][0] = agents[1][0] + 1
 else: 
-    y1 = y1 - 1
+    agents[1][0] = agents[1][0] - 1
     
 # alter x randomly by +-1
 if random.random() <0.5:
-    x1 = x1 + 1
+    agents[1][1] = agents[1][1] + 1
 else: 
-    x1 = x1 - 1
+    agents[1][1] = agents[1][1] - 1
 
 #TIMESTEP 5
 # alter y randomly by +-1 (sheep walk one step)
 if random.random() <0.5:
-    y1 = y1 + 1
+    agents[1][0] = agents[1][0] + 1
 else: 
-    y1 = y1 - 1
+    agents[1][0] = agents[1][0] - 1
     
 # alter x randomly by +-1
 if random.random() <0.5:
-    x1 = x1 + 1
+    agents[1][1] = agents[1][1] + 1
 else: 
-    x1 = x1 - 1
+    agents[1][1] = agents[1][1] - 1
 
 # CHECK
 print("after 5 timesteps")
 print("agent 0 is", agents[0][0], ",", agents[0][1])
-print("agent 1 is", y1, ",", x1)
+print("agent 1 is", agents[1][0], ",", agents[1][1])
 
 
 # distance between agents 0 and 1
 # pythag = a^2+b^2 = c^2
 
-euclid = ((math.pow((agents[0][1] - x1),2)) + (math.pow((agents[0][0]-y1),2)))**0.5            
+euclid = ((math.pow((agents[0][1] - agents[1][1]),2)) + (math.pow((agents[0][0]-agents[1][0]),2)))**0.5            
 print("agents 0 and 1 are", euclid, "units (euclidean distance) away from each other")
