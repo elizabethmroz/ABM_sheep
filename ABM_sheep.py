28818635e79ef6b637d3299a359a9a9e676c2a01
elizabethmroz <elizabethjmroz@gmail.com>
2022-02-01 11:11:17 +0000

    Tidied up code

--- ABM_sheep.py
+++ ABM_sheep.py
@@ -25,36 +25,50 @@ num_of_agents = 10
 # list created for coordinates
 agents = []
 
+# timesteps
+timestep = 10
+
+
 # for-loop creating agents
 for i in range(num_of_agents):
     agents.append([random.randint(0,99),random.randint(0,99)])
     
 
-# check
-print(agents)
-    
 
-# timesteps
-timestep = 10
 
     
 # for-loop to change x- and y-variable by timestep (Torus boundary solution)
 for x in range(timestep):
       
     for i in range(num_of_agents):
+        
         if random.random() < 0.5:
             agents[i][1] = (agents[i][1] + 1) % 100
         else: 
             agents[i][1] = (agents[i][1] - 1) % 100
             
-    for i in range(num_of_agents):
         if random.random() < 0.5:
             agents[i][0] = (agents[i][0] + 1) % 100
         else: 
             agents[i][0] = (agents[i][0] - 1) % 100
             
-    print(agents)
-    
+
+plt.ylim(0,99)
+plt.xlim(0,99)
+
+for i in range(num_of_agents):
+    plt.scatter(agents[i][0],agents[i][1], color='black')
+
+# most eastern point
+m_east = max(agents, key=operator.itemgetter(0))
+plt.scatter(m_east[0],m_east[1], color='blue')
+
+# most northern point
+m_north = max(agents, key=operator.itemgetter(1))
+plt.scatter(m_north[0],m_north[1], color='red')
+plt.show()
+
+
       
     
 '''
@@ -148,21 +162,6 @@ print("The coordinates of the agent located furthest north are", max(agents, key
 
 
 ''' 
-
-plt.ylim(0,99)
-plt.xlim(0,99)
-
-for i in range(num_of_agents):
-    plt.scatter(agents[i][0],agents[i][1], color='black')
-
-# most eastern point
-m_east = max(agents, key=operator.itemgetter(0))
-plt.scatter(m_east[0],m_east[1], color='blue')
-
-# most northern point
-m_north = max(agents, key=operator.itemgetter(1))
-plt.scatter(m_north[0],m_north[1], color='red')
-plt.show()
 
 
 
