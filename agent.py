5b8838b68d428290ce6034cee5eb01f088cb568f
elizabethmroz <elizabethjmroz@gmail.com>
2022-02-14 16:28:47 +0000

    Code fixed, agents move

--- agent.py
+++ agent.py
@@ -3,12 +3,20 @@ import operator
 import matplotlib.pyplot
 import agentframework
 
+
+# testing the connection works
+a = agentframework.Agent()
+print(a.y, a.x)
+a.move()
+print(a.y, a.x)
+
+
 def distance_between(agents_row_a, agents_row_b):
     return (((agents_row_a.x - agents_row_b.x)**2) +
-        ((agents_row_a.y - agents_row_b.y)**2))**0.5
+    ((agents_row_a.y - agents_row_b.y)**2))**0.5
 
 num_of_agents = 10
-num_of_iterations = 1
+num_of_iterations = 100
 agents = []
 
 
@@ -32,7 +40,6 @@ for i in range(num_of_agents):
 # Move the agents.
 for j in range(num_of_iterations):
     for i in range(num_of_agents):
-        
         agents[i].move()
         
 matplotlib.pyplot.xlim(0, 99)
@@ -57,6 +64,7 @@ for agents_row_a in agents:
          #   agents[i][1] = (agents[i][1] - 1) % 100
 
 
+'''
 matplotlib.pyplot.xlim(0, 99)
 matplotlib.pyplot.ylim(0, 99)
 for i in range(num_of_agents):
@@ -67,3 +75,4 @@ for agents_row_a in agents:
     for agents_row_b in agents:
         distance = distance_between(agents_row_a, agents_row_b)
 
+'''
